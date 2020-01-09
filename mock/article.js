a4bb4d06cfeba6e47962abc1e3ac273378ede1f7
Steve001 <steve0110001@gmail.com>
2020-01-09 08:08:15 +0800

    修复mock/article.js格式错乱问题

--- mock/article.js
+++ mock/article.js
@@ -28,107 +28,107 @@ for (let i = 0; i < count; i++) {
 }
 
 export default [{
-    url: '/article/list',
-    type: 'get',
-    response: config => {
-      const {
-        importance,
-        type,
-        title,
-        page = 1,
-        limit = 20,
-        sort
-      } = config.query
+  url: '/article/list',
+  type: 'get',
+  response: config => {
+    const {
+      importance,
+      type,
+      title,
+      page = 1,
+      limit = 20,
+      sort
+    } = config.query
 
-      let mockList = List.filter(item => {
-        if (importance && item.importance !== +importance) return false
-        if (type && item.type !== type) return false
-        if (title && item.title.indexOf(title) < 0) return false
-        return true
-      })
+    let mockList = List.filter(item => {
+      if (importance && item.importance !== +importance) return false
+      if (type && item.type !== type) return false
+      if (title && item.title.indexOf(title) < 0) return false
+      return true
+    })
 
-      if (sort === '-id') {
-        mockList = mockList.reverse()
-      }
-
-      const pageList = mockList.filter((item, index) => index < limit * page && index >= limit * (page - 1))
-
-      return {
-        code: 20000,
-        data: {
-          total: mockList.length,
-          items: pageList
-        }
-      }
+    if (sort === '-id') {
+      mockList = mockList.reverse()
     }
-  },
 
-  {
-    url: '/article/detail',
-    type: 'get',
-    response: config => {
-      const {
-        id
-      } = config.query
-      for (const article of List) {
-        if (article.id === +id) {
-          return {
-            code: 20000,
-            data: article
-          }
-        }
-      }
-    }
-  },
+    const pageList = mockList.filter((item, index) => index < limit * page && index >= limit * (page - 1))
 
-  {
-    url: '/article/pv',
-    type: 'get',
-    response: _ => {
-      return {
-        code: 20000,
-        data: {
-          pvData: [{
-              key: 'PC',
-              pv: 1024
-            },
-            {
-              key: 'mobile',
-              pv: 1024
-            },
-            {
-              key: 'ios',
-              pv: 1024
-            },
-            {
-              key: 'android',
-              pv: 1024
-            }
-          ]
-        }
-      }
-    }
-  },
-
-  {
-    url: '/article/create',
-    type: 'post',
-    response: _ => {
-      return {
-        code: 20000,
-        data: 'success'
-      }
-    }
-  },
-
-  {
-    url: '/article/update',
-    type: 'post',
-    response: _ => {
-      return {
-        code: 20000,
-        data: 'success'
+    return {
+      code: 20000,
+      data: {
+        total: mockList.length,
+        items: pageList
       }
     }
   }
+},
+
+{
+  url: '/article/detail',
+  type: 'get',
+  response: config => {
+    const {
+      id
+    } = config.query
+    for (const article of List) {
+      if (article.id === +id) {
+        return {
+          code: 20000,
+          data: article
+        }
+      }
+    }
+  }
+},
+
+{
+  url: '/article/pv',
+  type: 'get',
+  response: _ => {
+    return {
+      code: 20000,
+      data: {
+        pvData: [{
+          key: 'PC',
+          pv: 1024
+        },
+        {
+          key: 'mobile',
+          pv: 1024
+        },
+        {
+          key: 'ios',
+          pv: 1024
+        },
+        {
+          key: 'android',
+          pv: 1024
+        }
+        ]
+      }
+    }
+  }
+},
+
+{
+  url: '/article/create',
+  type: 'post',
+  response: _ => {
+    return {
+      code: 20000,
+      data: 'success'
+    }
+  }
+},
+
+{
+  url: '/article/update',
+  type: 'post',
+  response: _ => {
+    return {
+      code: 20000,
+      data: 'success'
+    }
+  }
+}
 ]
